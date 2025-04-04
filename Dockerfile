f2a075ae299e44dc128fe357aa7f327f255f72bb
Rohanvasista <rohanvasista6@gmail.com>
2025-04-05 04:47:35 +0530

    Initial commit - Dockerized Node.js App

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+﻿# Stage 1 - Build
+FROM node:18 AS builder
+
+WORKDIR /app
+COPY package*.json ./
+RUN npm install --omit=dev
+COPY . .
+
+# Stage 2 - Distroless
+FROM gcr.io/distroless/nodejs18
+
+WORKDIR /app
+COPY --from=builder /app .
+
+CMD ["index.js"]
+
---
+++ index.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Docker + GitHub + DockerHub!');
+});
+
+// Bind to 0.0.0.0 so Docker can expose it outside the container
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
